78a2d1f8dc9b75b333ae8edcb8e007383331cc36
Stalin Carrión <sfcarrion@utpl.edu.ec>
2018-08-30 09:50:48 -0500

    Texturas con fotografias reales, todavia en pruebas

---
+++ Muebles/Assets/Scribs/ChangeTexture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTexture : MonoBehaviour {
+
+ 
+    public Material material1;
+    bool flag = true;
+	// Use this for initialization
+	public void CambiarTextura () {
+        if (GameObject.FindGameObjectWithTag("suelo"))
+        {
+            GameObject suelo = GameObject.FindGameObjectWithTag("suelo");
+            //suelo.transform.GetComponent<MeshRenderer>().material.SetTexture("_MainTex",textura1);
+            suelo.transform.GetComponent<MeshRenderer>().material = material1;
+           Debug.Log("lo encontro");
+
+        }
+        else
+        {
+            Debug.Log("NOOOO");
+        }
+		
+	}
+	
+
+}
